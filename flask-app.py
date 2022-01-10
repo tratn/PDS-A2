7eb751bea5aeb58595e47717334422a1dbfcfd2e
Nguyen Tuan Dat <s3726128@student.rmit.edu.au>
2022-01-10 17:59:41 +0700

    update 3 models + flask

--- flask-app.py
+++ flask-app.py
@@ -1,9 +1,10 @@
 import os
+import re
 import pickle
 import pandas as pd
 import numpy as np
 
-from sklearn import preprocessing
+from sklearn.model_selection import StratifiedShuffleSplit 
 from sklearn.model_selection import KFold
 from flask import Flask, jsonify, request, render_template
 from sklearn.metrics import classification_report
@@ -11,190 +12,170 @@ from sklearn.metrics import confusion_matrix
 
 MODEL_PATH = "pickle_model.pkl"
 with open(MODEL_PATH, "rb") as rf:
-    clf = pickle.load(rf)
+    pkl = pickle.load(rf)
 
 # Preprocessing data
 
-
 def preprocessing_data():
-    # READ DATA
+    ### READ DATA
     url = 'survey_results_public.csv'
     preprocess = pd.read_csv(url, sep=',')
 
-    # MISSING VALUE
+    ### MISSING VALUE
     # list of mandatory columns.
     # 'RespodentID' is assigned automatically, hence not included in this list
     mandatory_cols = ['MainBranch', 'Hobbyist', 'Country',
                       'CurrencyDesc', 'CurrencySymbol', 'JobSeek']
     preprocess.dropna(subset=mandatory_cols, inplace=True)
+
     # drop missing values for CompFreq
     preprocess.dropna(subset=['CompFreq', 'CompTotal'], axis=0, inplace=True)
+    
+    # find the exchange rate for Danish krone 
     dkk_to_usd = 138936.0/80000.0
     converted_comp = preprocess.loc[preprocess.index.isin(
         [47224]), 'CompTotal']*dkk_to_usd
+
     # fill the missing converted compensation
     preprocess.loc[preprocess.index.isin(
         [47224]), 'ConvertedComp'] = converted_comp
+
     # fix the currency symbol
     preprocess.loc[preprocess.index.isin(
         [47224]), 'CurrencyDesc'] = 'Faroese krona'
     preprocess.loc[preprocess.index.isin([47224]), 'CurrencySymbol'] = 'KR'
+    
     # drop missing values for Cook Island dollar
     cols_to_drop = preprocess[preprocess.loc[:,
                                              'ConvertedComp'].isnull() == True].index.tolist()
     preprocess.drop(index=cols_to_drop, inplace=True)
+   
     # fill missing age and working hours with median
     med_age = preprocess['Age'].median()
     med_hour = preprocess['WorkWeekHrs'].median()
     preprocess.fillna({'Age': med_age, 'WorkWeekHrs': med_hour}, inplace=True)
+    
     # fill other columns with 'NotMentioned'
     cat_cols = preprocess.select_dtypes(include=[object]).columns.tolist()
     preprocess[cat_cols] = preprocess[cat_cols].fillna(value='NotMentioned')
 
-    # WHITESPACES/STRING MANIPULATION
+    ### WHITESPACES/STRING MANIPULATION
     # remove whitespace
     preprocess = preprocess.apply(
         lambda x: x.str.strip() if x.dtype == "object" else x)
     # transform letter to lowercase
     preprocess = preprocess.apply(
         lambda x: x.str.lower() if x.dtype == "object" else x)
+    
+    # Replace highest/lowest values with the corresponding float value
+    # Replace 'notmentioned' values with the MODE value
+    preprocess['Age1stCode'] = preprocess['Age1stCode'].replace("younger than 5 years", "4")
+    preprocess['Age1stCode'] = preprocess['Age1stCode'].replace("older than 85", "86")
+    preprocess['Age1stCode'] = preprocess['Age1stCode'].replace("notmentioned", "14")
 
-    # EXTREME VALUES
-    preprocess.drop(
-        preprocess.loc[preprocess['Age'] == 279].index, inplace=True)
-    preprocess.drop(
-        preprocess.loc[preprocess['WorkWeekHrs'] > (24*7)].index, inplace=True)
+    preprocess['YearsCode'] = preprocess['YearsCode'].replace("less than 1 year", "0.5")
+    preprocess['YearsCode'] = preprocess['YearsCode'].replace("more than 50 years", "51")
+    preprocess['YearsCode'] = preprocess['YearsCode'].replace("notmentioned", "10")
+
+    preprocess['YearsCodePro'] = preprocess['YearsCodePro'].replace("less than 1 year", "0.5")
+    preprocess['YearsCodePro'] = preprocess['YearsCodePro'].replace("more than 50 years", "51")
+    preprocess['YearsCodePro'] = preprocess['YearsCodePro'].replace("notmentioned", "3")
+    
+    # Cast 'Age1stCode', 'YearsCode', 'YearsCodePro' to float type
+    for col in ['Age1stCode', 'YearsCode', 'YearsCodePro']:
+        preprocess[col] = preprocess[col].astype('float64')
+
+    ## EXTREME VALUE AND OUTLIERS
+    preprocess.drop(preprocess[preprocess['Age1stCode'] > preprocess['Age']].index, inplace = True)
+    preprocess.drop(preprocess[preprocess['YearsCodePro'] > preprocess['YearsCode']].index, inplace = True)
+   
+    # Outliers
+    outliers_df = preprocess.loc[(preprocess['Age'] > 80) | (preprocess['Age1stCode'] > 75)]
+    preprocess.drop(outliers_df.index, inplace = True)
+    
+    # Impossible value
+    preprocess[preprocess['YearsCode'] > preprocess['Age']]
+    preprocess.loc[preprocess['YearsCode'] > preprocess['Age'], ['Age']] = preprocess['YearsCode'] + preprocess['Age1stCode']
+    preprocess.drop(preprocess.loc[preprocess['WorkWeekHrs'] > (24*7)].index, inplace=True)
+    
+    # generate copy of ConvertedComp column
+    preprocess["ConvertedComp_Copy"] = preprocess["ConvertedComp"]
+    
     # Create categories for compensation
     preprocess["ConvertedComp"] = pd.cut(preprocess["ConvertedComp"],
-                                         bins=[0, 25000, 50000,
-                                               100000, np.inf],
-                                         labels=['< 25k', '< 50k',
-                                                 '< 100k', '> 100k'],
+                                         bins=[0, 24000, 48000,
+                                               96000, np.inf],
+                                         labels=['0-24k', '24k-48k', '48k-96k', '>96k'],
                                          include_lowest=True)
 
-    # ENCODE TO NUMERIC VALUES
-    # 'YearsCode' and 'YearsCodePro' column
-    # drop non-meaning value
-    preprocess = preprocess[preprocess.YearsCode != 'notmentioned']
-    preprocess = preprocess[preprocess.YearsCodePro != 'notmentioned']
-    # convert columns to categories
-    preprocess['YearsCode'] = preprocess['YearsCode'].astype('category')
-    preprocess['YearsCodePro'] = preprocess['YearsCodePro'].astype('category')
-    # adding new categories
-    preprocess['YearsCode'] = preprocess['YearsCode'].cat.add_categories('0')
-    preprocess['YearsCode'] = preprocess['YearsCode'].cat.add_categories('51')
-    preprocess['YearsCodePro'] = preprocess['YearsCodePro'].cat.add_categories(
-        '0')
-    preprocess['YearsCodePro'] = preprocess['YearsCodePro'].cat.add_categories(
-        '51')
-    # classifying the suitable entries
-    preprocess.loc[(preprocess['YearsCode'].isin(
-        ["less than 1 year"])), ['YearsCode']] = "0"
-    preprocess.loc[(preprocess['YearsCode'].isin(
-        ["more than 50 years"])), ['YearsCode']] = "51"
-    preprocess.loc[(preprocess['YearsCodePro'].isin(
-        ["less than 1 year"])), ['YearsCodePro']] = "0"
-    preprocess.loc[(preprocess['YearsCodePro'].isin(
-        ["more than 50 years"])), ['YearsCodePro']] = "51"
-    # converting the columns' datatypes to numeric
-    preprocess["YearsCode"] = pd.to_numeric(
-        preprocess["YearsCode"], errors='raise')
-    preprocess["YearsCodePro"] = pd.to_numeric(
-        preprocess["YearsCodePro"], errors='raise')
+    ### CREATE NEW FEATURES 
+    def count_unique_value(row):
+        values = re.split(';', row)   
+        return len(values)
 
-    # Remove non-sense values
-    # remove outliers
-    preprocess = preprocess.loc[(preprocess['Age'] > 5)]
-    preprocess = preprocess.loc[(preprocess['Age'] < 80)]
-    # remove impossible value
-    preprocess = preprocess.loc[(
-        preprocess['YearsCode'] >= preprocess['YearsCodePro'])]
-    preprocess = preprocess.loc[(preprocess['Age'] > preprocess['YearsCode'])]
-    preprocess = preprocess.loc[(
-        preprocess['Age'] > preprocess['YearsCodePro'])]
-    preprocess = preprocess.loc[(preprocess['WorkWeekHrs'] < 24*7)]
+    preprocess['Database_Count'] = preprocess['DatabaseWorkedWith'].apply(lambda x: count_unique_value(x))
+    preprocess['Lang_Count'] = preprocess['LanguageWorkedWith'].apply(lambda x: count_unique_value(x))
+    preprocess['Misc_Count'] = preprocess['MiscTechWorkedWith'].apply(lambda x: count_unique_value(x))
+    preprocess['Platform_Count'] = preprocess['PlatformWorkedWith'].apply(lambda x: count_unique_value(x))
+    preprocess['Webframework_Count'] = preprocess['WebframeWorkedWith'].apply(lambda x: count_unique_value(x))
+    preprocess['Total_Count'] = preprocess['Database_Count'] + preprocess['Lang_Count'] + preprocess['Misc_Count'] + preprocess['Platform_Count'] + preprocess['Webframework_Count']
+    
+    # numerical features
+    num_feats = ['Age', 'Age1stCode', 'WorkWeekHrs', 'YearsCode', 'YearsCodePro', 'Lang_Count', 'Misc_Count', 'Platform_Count', 'Total_Count']
+    
+    # categorical features
+    cate_feats = ['MainBranch', 'Hobbyist', 'Country', 'EdLevel', 'Employment', 'JobSat',
+                 'JobSeek', 'NEWEdImpt', 'NEWLearn', 'NEWOffTopic', 'NEWOtherComms', 'NEWOvertime',
+                 'OpSys', 'OrgSize', 'PurchaseWhat', 'SOAccount', 'SOComm', 'SOPartFreq',
+                 'SOVisitFreq', 'UndergradMajor', 'WelcomeChange']
+    
+    # Cast some features to category types 
+    for col in cate_feats:
+        preprocess[col] = pd.Categorical(preprocess[col])
 
-    # 'NEWOvertime' column
-    # drop non-meaning value
-    preprocess = preprocess[preprocess.NEWOvertime != 'notmentioned']
-    # convert columns to categories
-    preprocess['NEWOvertime'] = preprocess['NEWOvertime'].astype('category')
-    # adding new categories
-    preprocess['NEWOvertime'] = preprocess['NEWOvertime'].cat.add_categories(
-        '0')
-    preprocess['NEWOvertime'] = preprocess['NEWOvertime'].cat.add_categories(
-        '1')
-    preprocess['NEWOvertime'] = preprocess['NEWOvertime'].cat.add_categories(
-        '2')
-    preprocess['NEWOvertime'] = preprocess['NEWOvertime'].cat.add_categories(
-        '3')
-    preprocess['NEWOvertime'] = preprocess['NEWOvertime'].cat.add_categories(
-        '4')
-    # classifying the suitable entries
-    preprocess.loc[(preprocess['NEWOvertime'].isin(["never"])),
-                   ['NEWOvertime']] = "0"
-    preprocess.loc[(preprocess['NEWOvertime'].isin(
-        ["rarely: 1-2 days per year or less"])), ['NEWOvertime']] = "1"
-    preprocess.loc[(preprocess['NEWOvertime'].isin(
-        ["occasionally: 1-2 days per quarter but less than monthly"])), ['NEWOvertime']] = "2"
-    preprocess.loc[(preprocess['NEWOvertime'].isin(
-        ["sometimes: 1-2 days per month but less than weekly"])), ['NEWOvertime']] = "3"
-    preprocess.loc[(preprocess['NEWOvertime'].isin(
-        ["often: 1-2 days per week or more"])), ['NEWOvertime']] = "4"
-    # converting the columns' datatypes to numeric
-    preprocess["NEWOvertime"] = pd.to_numeric(
-        preprocess["NEWOvertime"], errors='raise')
+    # others
+    othe_cols = ['DevType', 'JobFactors', 'LanguageWorkedWith', 'PlatformWorkedWith', 'MiscTechWorkedWith', 'DatabaseWorkedWith']
+    # merge all features to one dataframe
+    df_data = pd.concat([preprocess['ConvertedComp'], preprocess[num_feats], preprocess[cate_feats], preprocess[othe_cols]], axis=1)
 
-    # LABEL ENCODING FOR BETTER PREDICT
-    label_encoder = preprocessing.LabelEncoder()
-    # Transform data
 
-    def label_encode(column, dataset):
-        dataset[column] = label_encoder.fit_transform(dataset[column])
+    ### EXTRACT INFORMATION AND ENCODING
+    def split_value(dataframe, col):
+        values_dict = set()
 
-    # Encode labels in columns.
-    LabEnArr = ['Country', 'Employment', 'CompFreq',
-                'OpSys', 'OrgSize', 'EdLevel', 'MainBranch']
-    for i in LabEnArr:
-        label_encode(i, preprocess)
+        for index, row in dataframe.iterrows():
+            values = re.split(';', row[col])
+            for value in values:
+                values_dict.add(value)
+        return values_dict
 
-    # CREATE NEW FEATURES FROM 'DevType'
-    # method to check if the user have specific role that we want
-    def get_DevType(type, data):
-        column = 'DevType_' + type
-        data['DevType_' + type] = data['DevType'].copy()
-        data.loc[data[column].str.contains(type), column] = True
-        data[column] = np.where(data[column].isin([True]), data[column], False)
-        data[column] = data[column].astype(bool)
-    # choose which type of developer that we need
-    DevTypeArr = ['academic researcher', 'data or business analyst', 'data scientist or machine learning specialist', 'database administrator',
-                  'designer', 'developer', 'devops specialist', 'educator', 'engineer', 'engineering manager', 'marketing or sales professional',
-                  'product manager', 'scientist', 'senior executive', 'system administrator']
-    # create new features
-    for i in DevTypeArr:
-        get_DevType(i, preprocess)
-    # copy DevType features to new dataframe
-    DevType_df = preprocess.loc[:,
-                                preprocess.columns.str.startswith('DevType_')]
-
-    # MERGE DATESET WITH NEW FEATURES AND SPLIT THE DATASET FOR TESTING API
-    # split X, y
-    X = preprocess[['MainBranch', 'Country', 'Employment', 'CompFreq', 'OpSys', 'OrgSize', 'EdLevel', 'WorkWeekHrs',
-                    'Age', 'YearsCode', 'YearsCodePro', 'NEWOvertime']]
-    y = preprocess['ConvertedComp']
-    # merge devtype features with remaining data
-    X = pd.concat([X, DevType_df], axis=1)
-
+    ohe_cols = df_data.select_dtypes(include=['object']).columns.to_list()
+    
+    for col in ohe_cols:
+        ohe_col = split_value(df_data, col)
+        for val in ohe_col:
+            df_data[col + '_' + val] = np.where(df_data[col].str.contains(val, regex=False), 1, 0)
+    
+    df_data.drop(columns=ohe_cols, inplace=True)
+    
+    ### SPLIT THE DATASET FOR TESTING API
+    # split X and y
+    X = df_data.iloc[:, 1:]
+    y = df_data["ConvertedComp"]
+    
     return X, y
 
 
-def cross_val(X, y):
-    # KFold cross-validation for split data
-    kf = KFold(n_splits=5, random_state=42, shuffle=True)
-    for train_index, test_index in kf.split(X, y):
-        X_train, X_test = X.iloc[train_index], X.iloc[test_index]
-        y_train, y_test = y.iloc[train_index], y.iloc[test_index]
-    return X_test, y_test
+def train_test_split(X, y):
+    # create splitter function
+    splitter = StratifiedShuffleSplit(n_splits=1, random_state=42)
+    # get train and set dataset
+    for train,test in splitter.split(X, y):
+        X_train_SS = X.iloc[train]
+        y_train_SS = y.iloc[train]
+        X_test_SS = X.iloc[test]
+        y_test_SS = y.iloc[test]
+    return X_test_SS, y_test_SS
 
 
 # Init the app
@@ -208,35 +189,39 @@ def home():
 # predict function
 
 
-def predict_function(clf):
-    y_pred = clf.predict(X_test)
+def predict_function(pkl):
+    y_pred = pkl.predict(X_test_SS)
+
+    # make sure there are no "empty" dimensions since y_pred return 2d array
+    y_pred = np.squeeze(y_pred)
+    y_pred.ndim
+
     return y_pred
 
 
 # Predict function api
 @app.route("/predict", methods=['POST'])
 def predict():
-    predictions = predict_function(clf)
-
+    predictions = predict_function(pkl)    
     result = {
         'prediction': list(predictions)
     }
 
-    return result
+    return jsonify(result)
 
 
 # evaluate function
 def evaluate_function(model):
     # predict
-    y_pred = clf.predict(X_test)
+    y_pred = pkl.predict(X_test_SS)
 
     # evaluate
     from sklearn.metrics import accuracy_score
     from sklearn.metrics import precision_score
-    accuracy = accuracy_score(y_test, y_pred)
-    precision = precision_score(y_test, y_pred, average='macro')
-    report = classification_report(y_test, y_pred, output_dict=True)
-    matrix = confusion_matrix(y_test, y_pred)
+    accuracy = accuracy_score(y_test_SS, y_pred)
+    precision = precision_score(y_test_SS, y_pred, average='macro')
+    report = classification_report(y_test_SS, y_pred, output_dict=True)
+    matrix = confusion_matrix(y_test_SS, y_pred)
 
     return accuracy, precision, report, matrix
 
@@ -244,7 +229,7 @@ def evaluate_function(model):
 # evaluate function api
 @app.route("/evaluate", methods=['POST'])
 def evaluate():
-    accuracy, precision, report, matrix = evaluate_function(clf)
+    accuracy, precision, report, matrix = evaluate_function(pkl)
     matrix = pd.DataFrame(matrix).to_json(orient='values')
     result = {
         "accuracy": accuracy,
@@ -258,5 +243,5 @@ def evaluate():
 # main
 if __name__ == '__main__':
     X, y = preprocessing_data()
-    X_test, y_test = cross_val(X, y)
+    X_test_SS, y_test_SS = train_test_split(X, y)
     app.run(debug=True)
